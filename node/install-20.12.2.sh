b65baee102c0b37ad2c92485617a75fdd7e88655
Vladimir Galunshchikov <soyayaos@gmail.com>
2024-05-24 07:33:21 +0200

    feat: fs, node, nvm

---
+++ node/install-20.12.2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "installing NodeJS 20.12.2 ..."
+
+nvm install 20.12.2
+nvm alias default 20.12.2
+
+echo "done"
---
+++ nvm/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "installing nvm ..."
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash -
+
+echo "done"
